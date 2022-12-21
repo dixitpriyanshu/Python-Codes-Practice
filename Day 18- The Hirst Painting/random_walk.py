8bba21ddcb114136e52b607e7d5d5412c7e060c8
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-22 02:44:59 +0530

    random walk- random color added

--- Day 18- The Hirst Painting/random_walk.py
+++ Day 18- The Hirst Painting/random_walk.py
@@ -1,15 +1,23 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-timmy = Turtle()
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+t.colormode(255)
+
+timmy = t.Turtle()
+
+def random_color():
+    r =  random.randint(0, 255)
+    g =  random.randint(0, 255)
+    b =  random.randint(0, 255)
+    return (r, g, b)
+
 
 direction = [0, 90, 180, 270, 360]
 timmy.speed("fastest")
 timmy.pensize(15)
 
-for _ in range(200):
-    timmy.color(random.choice(colours))
+for _ in range(300):
+    timmy.color(random_color())
     timmy.forward(30)
     timmy.setheading(random.choice(direction))
 
