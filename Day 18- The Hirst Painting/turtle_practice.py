b9a13cff1a40b4df0cc3c088d3d54e6ef0e055ad
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-22 01:54:31 +0530

    turtle practice

---
+++ Day 18- The Hirst Painting/turtle_practice.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+for i in range(0,4):
+    timmy.forward(100)
+    timmy.right(90) 
+
+
+
+
+
+
+
+
+
+
+
+Screen = Screen()
+Screen.exitonclick()
