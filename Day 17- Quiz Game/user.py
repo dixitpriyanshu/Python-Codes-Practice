c97724de491d8d52ca4ee53c3c2d0595dc0e8404
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-21 23:26:55 +0530

    Self made Class edited v1

--- Day 17- Quiz Game/user.py
+++ Day 17- Quiz Game/user.py
@@ -3,8 +3,16 @@ class User:
     def __init__(self, userid, username) -> None:
         self.id = userid
         self.username = username
+        self.followers =0
+
 
 user_1 = User("001", "Priyanshu")
 
 print(user_1.id)
 print(user_1.username)
+
+print(user_1.followers)
+
+user_1.followers = 99
+
+print(user_1.followers)
