bded51b979522bd94d141f298e6394c8fa655321
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-22 16:14:45 +0530

    hirst painting finished

--- Day 18- The Hirst Painting/main.py
+++ Day 18- The Hirst Painting/main.py
@@ -1,16 +1,3 @@
-# import colorgram
-
-# image = 'Day 18- The Hirst Painting/image.jpg'
-# rgb_colors = []
-# colors = colorgram.extract(image, 30)
-
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-
 import turtle as t
 import random
 
