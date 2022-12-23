65438c9c69251fd817d90d2be15a5e09a53154a0
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-24 01:17:01 +0530

    Pong Game Finish

--- Day 22 - Build Pong- The famous Arcade Game/main.py
+++ Day 22 - Build Pong- The famous Arcade Game/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -12,17 +13,37 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = ScoreBoard()
 
 screen.listen()
-screen.onkey(r_paddle.go_up,"Up")
-screen.onkey(r_paddle.go_down,"Down")
-screen.onkey(l_paddle.go_up,"w")
-screen.onkey(l_paddle.go_down,"s")
+screen.onkeypress(r_paddle.go_up,"Up")
+screen.onkeypress(r_paddle.go_down,"Down")
+screen.onkeypress(l_paddle.go_up,"w")
+screen.onkeypress(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+    #Detect collision with wallm and bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detect collision with paddle
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) <  50 and ball.xcor() < -320):
+        ball.bounce_x ()
+    # Detect right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    # Detect left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+    
 
 screen.exitonclick()
