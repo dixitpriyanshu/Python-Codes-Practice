a6b02a45a106ac1ea2b6210f19c838bbc6427f07
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-24 19:02:02 +0530

    updating Snake Game

---
+++ Day 24 - Working with Files, Directories and Paths/Updating Snake Game/scoreboard.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 22, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.highscore = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 265)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+    
+    def reset(self) -> None:
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
