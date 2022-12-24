823834491e20c155182567c18d11682fb1678fb8
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-24 20:31:54 +0530

    Mail Merge Finished

--- Day 24 - Working with Files, Directories and Paths/Mail Merge Challenge/main.py
+++ Day 24 - Working with Files, Directories and Paths/Mail Merge Challenge/main.py
@@ -1,8 +1,14 @@
-#Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+PLACEHOLDER = "[name]"
+
+with open("Day 24 - Working with Files, Directories and Paths/Mail Merge Challenge/Input/Names/invited_names.txt") as name_data:
+    names = name_data.readlines()
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Day 24 - Working with Files, Directories and Paths/Mail Merge Challenge/Input/Letters/starting_letter.txt") as sample_letter:
+        letter_contents = sample_letter.read()
+        for name in names:
+            stripped_name = name.strip("\n")
+            new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+            with open(f"Day 24 - Working with Files, Directories and Paths/Mail Merge Challenge/Output/ReadyToSend/letter_to_{stripped_name}.txt","w") as final_letter:
+                final_letter.write(new_letter)
+        
