7eda8a09e81869233a0d7859e5ebe7606eea8803
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-26 00:54:01 +0530

    Day 27 debugged

--- Day 27 - Tkinter GUI Program/main.py
+++ Day 27 - Tkinter GUI Program/main.py
@@ -1,21 +1,21 @@
-from tkinter import *
+import tkinter as tk
 
 def convert():
     miles = int(miles_entry.get())
     km = miles * 1.6
     answer_label.config(text= f"{km}")
 
-root = Tk()
+root = tk.Tk()
 root.title("Converter")
 root.minsize(width= 100, height= 80)
 root.config(padx= 20,pady= 20)
 
-miles_label = Label(text= "miles")
-km_label = Label(text= "km")
-equal_label = Label(text= "is equal to")
-answer_label = Label(text= "0")
-miles_entry = Entry(width= 10)
-calculate_button = Button(text= "Calculate", command=convert)
+miles_label = tk.Label(text= "miles")
+km_label = tk.Label(text= "km")
+equal_label = tk.Label(text= "is equal to")
+answer_label = tk.Label(text= "0")
+miles_entry = tk.Entry(width= 10)
+calculate_button = tk.Button(text= "Calculate", command=convert)
 
 miles_entry.grid(column= 1, row= 0)
 miles_label.grid(column= 2, row= 0)
--- Day 27 - Tkinter GUI Program/practice.py
+++ Day 27 - Tkinter GUI Program/practice.py
@@ -1,15 +1,15 @@
-from tkinter import *
+import tkinter as tk
 
 def button_clicked():
     my_label.config(text= f"hii! {input.get()}")
 
-window = Tk()
+window = tk.Tk()
 window.title("My First GUI Program")
 window.minsize(width= 500, height=300)
 window.config(padx= 100,pady= 200)
 
 ## Label
-my_label = Label(text= "I am a Label", font= ("Arial", 24, "bold"))
+my_label = tk.Label(text= "I am a Label", font= ("Arial", 24, "bold"))
 my_label.grid(column= 0, row= 0)
 my_label.config(padx= 50,pady= 50)
 
@@ -18,14 +18,14 @@ my_label.config(padx= 50,pady= 50)
 my_label.config(text= "Hii")
 
 ########  BUtton
-button = Button(text= "Click Me", command= button_clicked)
+button = tk.Button(text= "Click Me", command= button_clicked)
 button.grid(column= 1, row= 1)
 
 ######    Entry
-input = Entry(width= 10)
+input = tk.Entry(width= 10)
 input.grid(column= 3, row= 2)
 
-new_button = Button(text= "Don't Click Me")
+new_button = tk.Button(text= "Don't Click Me")
 new_button.grid(column= 2,row= 0)
 
 
