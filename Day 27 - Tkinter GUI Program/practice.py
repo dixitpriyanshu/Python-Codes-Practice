399aa19a6419d7dbeefcb6c65dce117dea66b248
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-25 19:16:10 +0530

    tkinter demo

--- Day 27 - Tkinter GUI Program/practice.py
+++ Day 27 - Tkinter GUI Program/practice.py
@@ -1,6 +1,6 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("My First GUI Program")
 window.minsize(width= 500, height=300)
@@ -8,8 +8,24 @@ window.minsize(width= 500, height=300)
 
 ## Label
 
-my_label = tkinter.Label(text= "I am a Label", font= ("Arial", 24, "bold"))
+my_label = Label(text= "I am a Label", font= ("Arial", 24, "bold"))
 my_label.pack()
+
+# my_label["text"] = New Text"
+# Alternate
+my_label.config(text= "What's your name?")
+
+def button_clicked():
+    my_label.config(text= f"hii! {input.get()}")
+
+########  BUtton
+button = Button(text= "Click Me", command= button_clicked)
+button.pack()
+
+######    Entry
+
+input = Entry(width= 10)
+input.pack()
 
 
 
