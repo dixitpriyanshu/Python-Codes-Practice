2c3e9bd49e91aa5208668b8190925b0ccec1bb57
dixitpriyanshu <priyanshudixit2910@gmail.com>
2022-12-26 01:10:59 +0530

    Password manager start

---
+++ Day 29 - Password Manager/main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window =  tk.Tk()
+window.title("Password Manager")
+window.config(padx= 20, pady= 20)
+
+canvas = tk.Canvas(width= 200, height= 200, highlightthickness= 0)
+
+lock_img = tk.PhotoImage(file= "Day 29 - Password Manager/logo.png")
+canvas.create_image(100, 100, image= lock_img)
+canvas.pack()
+
+
+
+
+
+window.mainloop()
