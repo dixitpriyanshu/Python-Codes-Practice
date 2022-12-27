9886aa2135fc01d5f7e1fd8681a0591bc1356417
Priyanshu Dixit <priyanshudixit2910@gmail.com>
2022-12-27 21:20:39 +0530

    Flash Card App Start

---
+++ Day 31 - Flash Card/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+# -------------- CONSTANTS ------------#
+BACKGROUND_COLOR = "#B1DDC6"
+#------------------ UI -----------------#
+window = tk.Tk()
+
+
+
+
+
+
+window.mainloop()
