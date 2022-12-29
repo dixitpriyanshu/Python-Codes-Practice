60ac1fb5f29bb38f3cf2a1b987f1d547a8105366
Priyanshu Dixit <priyanshudixit2910@gmail.com>
2022-12-29 22:21:00 +0530

    Kanye quotes complete

--- Day 33- ISS Overhead Notifier/kanye_quote.py
+++ Day 33- ISS Overhead Notifier/kanye_quote.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-
+import requests
 
 def get_quote():
-    pass
-    #Write your code here.
-
+    response = requests.get(url = "https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text = quote)
 
 
 window = tk.Tk()
@@ -20,7 +22,7 @@ canvas.grid(row=0, column=0)
 kanye_img = tk.PhotoImage(file="Day 33- ISS Overhead Notifier/kanye.png")
 kanye_button = tk.Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
+get_quote()
 
 
 window.mainloop()
