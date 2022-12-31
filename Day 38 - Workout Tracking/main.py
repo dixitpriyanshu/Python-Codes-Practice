76e056f48cfc9b845cd23787d8c745dfb65e3a00
Priyanshu Dixit <priyanshudixit2910@gmail.com>
2022-12-31 19:19:13 +0530

    Workout Tracker

---
+++ Day 38 - Workout Tracking/main.py
@@ -0,0 +1,51 @@
+import requests
+from datetime import datetime
+
+APP_ID = "227f503d"
+API_KEY = "e667c98a021ab3a44bb94d9cbd664ec3"
+GENDER = "male"
+WEIGHT = 54
+HEIGHT = 172
+AGE = 22
+
+nutritionnix_exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/4d7224b59773f15d1eaf1ecec2a224e5/workoutTracking/workouts"
+nutritionnix_header = {
+    "x-app-id" : APP_ID,
+    "x-app-key" :API_KEY
+}
+
+exercise_input = input("Tell me which exercise you did: ")
+
+nutritionnix_parameters = {
+    "query" : exercise_input,
+    "gender" : GENDER,
+    "weight_kg" : WEIGHT,
+    "height_cm" : HEIGHT,
+    "age" :  AGE
+}
+
+response = requests.post(
+    url = nutritionnix_exercise_endpoint,
+    json = nutritionnix_parameters,
+    headers = nutritionnix_header
+)
+
+result = response.json()["exercises"]
+print(result)
+today = datetime.now()
+date = today.strftime("%d/%m/%Y")
+time = today.strftime("%X")
+
+for exercise in result:
+    sheety_parameters = {
+        "workout" : {
+        "date" : date,
+        "time" : time,
+        "exercise" :exercise["name"].title(),
+        "duration" : exercise["duration_min"],
+        "calories" : exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(url= sheety_endpoint, json= sheety_parameters)
+    print(sheet_response.text)
