80661cfb4dcdccf867e09c565ae0f422239a6c4b
Priyanshu Dixit <priyanshudixit2910@gmail.com>
2023-01-10 22:12:26 +0530

    Update internetSpeedTwitterBot.py

--- Day 50 - Twitter Bot/internetSpeedTwitterBot.py
+++ Day 50 - Twitter Bot/internetSpeedTwitterBot.py
@@ -1,6 +1,5 @@
 import time
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
